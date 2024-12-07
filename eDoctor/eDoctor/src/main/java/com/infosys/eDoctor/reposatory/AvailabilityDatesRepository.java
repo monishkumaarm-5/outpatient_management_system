3a0019954a776678b16d624f367420d2f77001ce
zap08 <hellxxxfurry@gmail.com>
2024-12-07 17:29:51 +0530

    Delete eDoctor directory

--- eDoctor/eDoctor/src/main/java/com/infosys/eDoctor/reposatory/AvailabilityDatesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infosys.eDoctor.reposatory;
-
-import com.infosys.eDoctor.entity.AvailabilityDates;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvailabilityDatesRepository extends JpaRepository<AvailabilityDates, Integer> {
-}
